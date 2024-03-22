bc26eeb37147dcaf36b070d2423d90cae1133bbf
v sotnikov <sotnikov@rosplatforma.ru>
2024-03-22 12:21:29 +0300

    22032024commit

---
+++ src/Solution.java
@@ -0,0 +1,15 @@
+import java.util.concurrent.ConcurrentSkipListMap;
+
+/*
+Найти класс по описанию Ӏ Java Collections: 7 уровень, 6 лекция
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getExpectedClass());
+    }
+
+    public static Class getExpectedClass() {
+        return ConcurrentSkipListMap.class;
+    }
+}
